2404546d455f204d950f2bb21337bf739e9e05a4
Kim-Flores <kimflores.k@gmail.com>
2025-03-25 01:03:07 -0400

    adjust img size & page layout

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -9,13 +9,19 @@ const About = () => {
           className="hover:animate-pulse"
           src="./works/spaceBunny.png"
           alt="Womp Model"
-          width={200}
+          width={250}
           height={150}
         />
       </div>
-      <div className="flex p-3 justify-center">
-        <p className="p-2 bg-white/60 dark:bg-black/50 rounded-lg">Hello! :D</p>
+      <div className="flex pt-8 px-2 justify-center">
+        <Link
+          href="/works"
+          className="p-2 bg-white/60 hover:bg-white/70 rounded dark:bg-black/50 dark:hover:bg-black/70"
+        >
+          My Portfolio
+        </Link>
       </div>
+
       <div className="my-6">
         <p className="text-4xl font-bold">Kim Flores</p>
         <p>Artist & Developer | Based in Boston</p>
@@ -34,14 +40,7 @@ const About = () => {
         </div>
       </div>
       <div className="mb-6">I &#9829; dogs, music, art & boba tea</div>
-      <div className="flex justify-center">
-        <Link
-          href="/works"
-          className="p-2 bg-white/60 hover:bg-white/70 rounded dark:bg-black/50 dark:hover:bg-black/70"
-        >
-          My Portfolio
-        </Link>
-      </div>
+      <div className="flex justify-center"></div>
     </div>
   );
 };
